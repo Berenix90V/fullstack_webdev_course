a9f0e07c68f21f5bd002e5ec81b83d108738ffad
Veronica Zanella <zanella.veronica@outlook.it>
2023-06-27 23:51:56 +0300

    exercise 1.4

--- part_1/course_information/src/App.js
+++ part_1/course_information/src/App.js
@@ -16,9 +16,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part = {props.part1} />
-            <Part part = {props.part2}  />
-            <Part part = {props.part3}  />
+            <Part part = {props.parts[0]} />
+            <Part part = {props.parts[1]}  />
+            <Part part = {props.parts[2]}  />
         </>
     )
 }
@@ -29,26 +29,23 @@ const Total = (props) => {
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [{
       name: 'Fundamentals of React',
       exercises: 10
-  }
-  const part2 = {
+  },
+  {
       name: 'Using props to pass data',
       exercises: 7
-  }
-  const part3 = {
+  },
+ {
       name:'State of a component',
       exercises: 14
-  }
+  }]
   return(
       <div>
         <Header course = {course} />
-        <Content
-            part1 = {part1}
-            part2 = {part2}
-            part3 = {part3}  />
-        <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises} />
+        <Content parts = {parts} />
+        <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises} />
       </div>
   )
 }
