8c495bd7cf5ad9d85fefb5628b70a6f46c711190
Veronica Zanella <zanella.veronica@outlook.it>
2023-06-27 19:24:44 +0300

    modified div into fragment

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -2,20 +2,20 @@
 const Hello = (props) => {
     console.log(props)
     return(
-        <div>
+        <>
             <p>Hello {props.name}, you are {props.age} years old</p>
-        </div>
+        </>
     )
 }
 const App = () => {
     const name = 'Peter'
     const age = 24
     return (
-        <div>
+        <>
             <h1>Greetings</h1>
             <Hello name={'George'} age={26+10} />
             <Hello name={name} age={age}/>
-        </div>
+        </>
     )
 }
 
