db9b9d4b51c1e26ca7ad3659cbe1c98d3daa07c5
Veronica Zanella <zanella.veronica@outlook.it>
2023-06-28 13:34:42 +0300

    function inside function

--- part_1/theory/src/App.js
+++ part_1/theory/src/App.js
@@ -1,11 +1,29 @@
 
+const Hello =(props) => {
+    const bornYear = () => {
+        const yearNow = new Date().getFullYear()
+        return yearNow - props.age
+    }
+    return(
+        <div>
+            <p>
+                Hello {props.name}, you are {props.age} years old
+            </p>
+            <p>
+                So you were probably born in{bornYear()}
+            </p>
+        </div>
+    )
+}
 const App = () => {
-    const friends = ['Peter', 'Maya']
+    const name = 'Peter'
+    const age = 10
     return (
-        <>
+        <div>
             <h1>Greetings</h1>
-            <p>{friends}</p>
-        </>
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={name} age={age} />
+        </div>
     )
 }
 
