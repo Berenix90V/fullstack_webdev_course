0924f9765c67b03fafd7524459ba0a834797b045
Veronica Zanella <zanella.veronica@outlook.it>
2023-06-28 15:20:42 +0300

    components re-render

--- part_1/theory/src/App.js
+++ part_1/theory/src/App.js
@@ -13,12 +13,14 @@ const Hello =({name, age}) => {
         </div>
     )
 }
-const App = () => {
+const App = (props) => {
+    const counter = props.counter
     const name = 'Peter'
     const age = 10
     return (
         <div>
             <h1>Greetings</h1>
+            <p>{counter}</p>
             <Hello name="Maya" age={26 + 10} />
             <Hello name={name} age={age} />
         </div>
