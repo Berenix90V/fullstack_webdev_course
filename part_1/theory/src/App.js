00685765b08fd24abd689a73a90f0d49eecd2733
Veronica Zanella <zanella.veronica@outlook.it>
2023-06-29 20:08:01 +0300

    functions refactor

--- part_1/theory/src/App.js
+++ part_1/theory/src/App.js
@@ -5,21 +5,16 @@ const App = () => {
         left: 0,
         right: 0
     })
-    const handleLeftClick = () => {
-        const newClicks = {
+    const handleLeftClick = () => setClicks({
             ...clicks,
             left: clicks.left +1
-        }
-        setClicks(newClicks)
-    }
+        })
 
-    const handleRightClick = () => {
-        const newClicks = {
+    const handleRightClick = () => setClicks({
             ...clicks,
             right: clicks.right +1
-        }
-        setClicks(newClicks)
-    }
+        })
+
 
 
     return(
