f781a93b6e15704b4807a64761fe7b6979e295e6
Veronica Zanella <zanella.veronica@outlook.it>
2023-06-30 12:28:04 +0300

    function that returns a function

--- part_1/theory/src/App.js
+++ part_1/theory/src/App.js
@@ -1,56 +1,17 @@
 import {useState} from 'react'
 
-const History = (props) => {
-    if(props.allClicks.length === 0){
-        return(
-            <div>
-                the app is used clicking buttons
-            </div>
-        )
-    }
-    else{
-        return(
-            <div>
-                button press history: {props.allClicks.join(' ')}
-            </div>
-        )
-    }
-}
-
-const Button = ({handleClick, text}) => {
-    return(
-        <button onClick={handleClick}>{text}</button>
-    )
-}
-
 const App = () => {
-    const [leftClick, setLefClick] = useState(0)
-    const [rightClick, setRightClick] = useState(0)
-    const [allClicks, setAll] = useState([])
-    const [total, setTotal] = useState(0)
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        const updateLeft = leftClick+1
-        setLefClick(updateLeft)
-        setTotal(updateLeft+rightClick)
+    const [value, setValue] = useState(10)
+    const hello = () => {
+        const handler = () => console.log('Hello')
+        return handler
     }
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        const updateRight = rightClick+1
-        setRightClick(updateRight)
-        setTotal(leftClick+updateRight)
-    }
-
     return(
         <div>
-            {leftClick}
-            <Button handleClick={handleLeftClick} text='left' />
-            <Button handleClick={handleRightClick} text='right' />
-            {rightClick}
-            <History allClicks={allClicks}/>
-            <p>Total: {total}</p>
+            {value}
+            <button onClick={hello()} >button</button>
         </div>
+
     )
 }
 
