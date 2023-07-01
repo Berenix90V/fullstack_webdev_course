09f3608e25edb13752a03f66306bda5f0d4da6f9
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-01 13:04:06 +0300

    exercise 1.7

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -4,10 +4,12 @@ const Title = ({text}) => <h1>{text}</h1>
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 const DisplaySingleStat = ({text, value}) => <p>{text}: {value}</p>
 function App() {
-  const [good, setGood] = useState(0)
+    const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const total = good+neutral+bad
+  const average = total === 0? 0 : (good-bad)/total
+  const positive = total === 0? 0: good*100/total
   return (
     <div >
         <Title text='Give your feedback' />
@@ -16,8 +18,11 @@ function App() {
         <Button handleClick={()=>setBad(bad+1)} text='Bad' />
         <Title text='Statistics' />
         <DisplaySingleStat text='good' value={good} />
-        <DisplaySingleStat text='neutral' value={neutral}/>
-        <DisplaySingleStat text='bad' value={bad}/>
+        <DisplaySingleStat text='neutral' value={neutral} />
+        <DisplaySingleStat text='bad' value={bad} />
+        <DisplaySingleStat text='all' value={total} />
+        <DisplaySingleStat text='average' value={average} />
+        <DisplaySingleStat text='positive' value={positive.toString()+' %'} />
     </div>
   );
 }
