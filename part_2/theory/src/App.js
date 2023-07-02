d9dee8214c6214597e39273f1883adaba691181b
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-02 12:01:31 +0300

    on submit prevent default

--- part_2/theory/src/App.js
+++ part_2/theory/src/App.js
@@ -1,14 +1,25 @@
 import Note from "./components/Note";
+import {useState} from "react";
 
-const App = ({notes}) => {
-  return (
-     <div>
-       <h1>Notes</h1>
-       <ul>
-           {notes.map((note)=> <Note key={note.id} note={note}/>)}
-       </ul>
-     </div>
-  );
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+    const addNote = (event)=>{
+        event.preventDefault()
+        console.log('button clicked', event.target)
+    }
+    return (
+         <div>
+            <h1>Notes</h1>
+            <ul>
+               {notes.map((note)=> <Note key={note.id} note={note}/>)}
+            </ul>
+            <form onSubmit={addNote}>
+                <input/>
+                <button type="submit">save</button>
+            </form>
+
+         </div>
+    );
 }
 
 /*
