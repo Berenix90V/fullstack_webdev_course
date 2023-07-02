979fc47b1a11451e1eafe6cd7b8fd074c7060948
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-02 17:52:06 +0300

    refactored chained method calls

--- part_2/theory/src/index.js
+++ part_2/theory/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios'
 
-axios.get('http://localhost:3001/notes').then(response => {
+axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
     const notes = response.data
     console.log(notes)
 })
