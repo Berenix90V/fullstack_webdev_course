c7b64ebeab70937fdcdbe54d07aa88cc37e0b8a0
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-02 15:26:29 +0300

    exercise 2.6

---
+++ part_2/phonebook/src/App.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+const App = () => {
+    const [persons, setPersons] = useState([{name:'Arto Hellas'}])
+    const [newName, setNewName] = useState('')
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+    const addName = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName
+        }
+        setPersons(persons.concat(personObject))
+    }
+    return (
+    <div className="App">
+        <h2>Phonebook</h2>
+        <form onSubmit={addName}>
+            <div>
+                name: <input onChange={handleNameChange} value={newName}/>
+            </div>
+            <div>
+              <button type="submit">add</button>
+            </div>
+        </form>
+        <h2>Numbers</h2>
+        <div>
+            {persons.map((person)=><p key={person.name}>{person.name}</p>)}
+        </div>
+    </div>
+    );
+}
+
+export default App;
