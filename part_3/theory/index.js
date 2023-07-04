541829a4ffc1d493c21da7d3521f3c7ba8c105b2
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-04 17:48:18 +0300

    installed express and substituted the built-in server

--- part_3/theory/index.js
+++ part_3/theory/index.js
@@ -1,4 +1,4 @@
-import http from 'http'
+import express from 'express'
 
 let notes = [
     {
@@ -18,9 +18,14 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200,{'Content-Type': 'application/json'})
-    response.end(JSON.stringify(notes))
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
 const PORT = 3001
