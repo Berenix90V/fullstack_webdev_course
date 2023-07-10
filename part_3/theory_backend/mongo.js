dd6b3d6bcb8db65a1ef4f64518236cb989b3bf1a
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-10 17:23:23 +0300

    return all objects Note in database

--- part_3/theory_backend/mongo.js
+++ part_3/theory_backend/mongo.js
@@ -20,6 +20,7 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+/*
 const note = new Note({
     content: 'Mongoose make things easy',
     important: true,
@@ -28,5 +29,13 @@ const note = new Note({
 
 note.save().then(result => {
     console.log('note saved!')
+    mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
     mongoose.connection.close()
 })
