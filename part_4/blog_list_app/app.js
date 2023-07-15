ec4926ed0ef1c18cc28de2aa1b8503a15786db05
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-15 18:44:25 +0300

    moved routes in a separate file in controllers

--- part_4/blog_list_app/app.js
+++ part_4/blog_list_app/app.js
@@ -2,7 +2,7 @@ import config from './utils/config.js'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import Blog from "./models/blog.js";
+import blogsRouter from './controllers/blogs.js'
 
 const app = express()
 
@@ -11,21 +11,6 @@ mongoose.connect(config.mongodb_url)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 export default app
