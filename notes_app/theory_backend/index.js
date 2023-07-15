faa1a8359cca266486a90bae79e8edd77f0beaa4
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-15 15:30:28 +0300

    reduced index file to simply call the app from app.js

---
+++ notes_app/theory_backend/index.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+import app from './app.js'
+import config from './utils/config'
+import logger from './utils/logger.js'
+
+app.listen(config.PORT, () => {
+    logger.info(`Server listening on the port ${config.PORT}`)
+})
