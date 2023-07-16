377ccc90a6f9e54359504275afa9b93e4af4708f
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-16 13:29:32 +0300

    exercise 4.4

--- part_4/blog_list_app/tests/list_helper.test.js
+++ part_4/blog_list_app/tests/list_helper.test.js
@@ -5,3 +5,51 @@ test('dummy returns one', () => {
     const result = list_helper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const listWithMoreBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f3',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 8,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17g3',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 1,
+            __v: 0
+        }
+    ]
+    test('of a list with a single blog', () => {
+        expect(list_helper.totalLikes(listWithOneBlog)).toBe(5)
+    })
+    test('of a list with more blogs', () => {
+        expect(list_helper.totalLikes(listWithMoreBlogs)).toBe(14)
+    })
+    test('of an empty list', () => {
+        expect(list_helper.totalLikes([])).toBe(0)
+    })
+})
--- part_4/blog_list_app/utils/list_helper.js
+++ part_4/blog_list_app/utils/list_helper.js
@@ -1,6 +1,12 @@
 const dummy = (blogs) => {
     return 1
 }
+const totalLikes = (blogs) => {
+    const sum = (total, blog) => {
+        return total + blog.likes
+    }
+    return blogs.length === 0? 0 : blogs.reduce(sum,0)
+}
 
-const list_helper = {dummy}
+const list_helper = {dummy, totalLikes}
 export default list_helper
