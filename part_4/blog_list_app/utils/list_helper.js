2a33c3f8ed9e840576bb777f243b33592f9bd87d
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-16 15:28:09 +0300

    exercise 4.5

--- part_4/blog_list_app/utils/list_helper.js
+++ part_4/blog_list_app/utils/list_helper.js
@@ -8,5 +8,27 @@ const totalLikes = (blogs) => {
     return blogs.length === 0? 0 : blogs.reduce(sum,0)
 }
 
-const list_helper = {dummy, totalLikes}
+const favoriteBlog = (blogs) => {
+    const findFavBlog = (fav, blog) => {
+        return fav.likes > blog.likes? fav : blog
+    }
+
+    const favBlog = blogs.length === 0? null : blogs.reduce(findFavBlog, 0)
+    if (favBlog) {
+        return {
+            title:favBlog.title,
+            author:favBlog.author,
+            likes: favBlog.likes
+        }
+    } else {
+        return null
+    }
+
+}
+
+const list_helper = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
 export default list_helper
