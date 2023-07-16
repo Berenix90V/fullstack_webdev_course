1e4cefecf49aa769a34041df1714eb7c330d6687
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-16 20:08:29 +0300

    added possibility to switch db according to NODE_ENV

--- notes_app/theory_backend/utils/config.js
+++ notes_app/theory_backend/utils/config.js
@@ -1,12 +1,10 @@
 import 'dotenv/config'
 
 const PORT = process.env.PORT
-const MONGODB_URL = process.env.MONGO_URL
-const TEST_MONGO_URL = process.env.TEST_MONGO_URL
+const MONGODB_URL = process.env.NODE_ENV==='test'? process.env.TEST_MONGO_URL : process.env.MONGO_URL
 
 const config = {
     port: PORT,
     mongodb_url: MONGODB_URL,
-    test_mongodb_url: TEST_MONGO_URL
 }
 export default config
