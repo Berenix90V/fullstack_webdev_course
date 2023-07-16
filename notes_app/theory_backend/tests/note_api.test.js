ae8b91afd1401d75e81af085163c67a46e951357
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-16 22:13:42 +0300

    modified the test to have always the same database status

--- notes_app/theory_backend/tests/note_api.test.js
+++ notes_app/theory_backend/tests/note_api.test.js
@@ -1,8 +1,28 @@
 import mongoose from 'mongoose'
 import supertest from 'supertest'
-import app from "../app.js";
+import app from '../app.js'
+import Note from '../models/note.js'
 
 const api=supertest(app)
+
+const initialNotes = [
+    {
+        content: 'HTML is easy',
+        important: false,
+    },
+    {
+        content: 'Browser can execute only JavaScript',
+        important: true,
+    }
+]
+
+beforeEach(async() => {
+    await Note.deleteMany({})
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
     await api
@@ -14,11 +34,13 @@ test('notes are returned as json', async () => {
 test('there are two notes', async () => {
     const response = await api.get('/api/notes')
 
-    expect(response.body).toHaveLength(2)
+    expect(response.body).toHaveLength(initialNotes.length)
 })
 
-test('the first note is about HTTP methods', async () => {
+test('a specific note is within the returned notes', async () => {
     const response = await api.get('/api/notes')
+    const contents = response.body.map(r => r.content)
+    expect(contents).toContain('Browser can execute only JavaScript')
 
     expect(response.body[0].content).toBe('HTML is easy')
 })
