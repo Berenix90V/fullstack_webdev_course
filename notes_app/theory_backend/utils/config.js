42d3790935dd3d7753d28d8494c872a5b167109f
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-16 19:18:33 +0300

    updated dotenv variables and config file to have different database in test and production

--- notes_app/theory_backend/utils/config.js
+++ notes_app/theory_backend/utils/config.js
@@ -2,9 +2,11 @@ import 'dotenv/config'
 
 const PORT = process.env.PORT
 const MONGODB_URL = process.env.MONGO_URL
+const TEST_MONGO_URL = process.env.TEST_MONGO_URL
 
 const config = {
     port: PORT,
-    mongodb_url: MONGODB_URL
+    mongodb_url: MONGODB_URL,
+    test_mongodb_url: TEST_MONGO_URL
 }
 export default config
