9ed763c1a5432db1f9f1164f32cb970920753999
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-18 00:16:54 +0300

    exercise 4.13: implemented test for delete method

--- part_4/blog_list_app/tests/blog_api.test.js
+++ part_4/blog_list_app/tests/blog_api.test.js
@@ -16,85 +16,102 @@ beforeEach(async() => {
     }
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('view all blogs', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+    test('verify existence of unique property id', async () => {
+        const response = await api.get('/api/blogs')
+        const blogs = response.body
+        for(const blog of blogs){
+            expect(blog.id).toBeDefined()
+        }
+        const ids = _.countBy(blogs,blog=>blog.id)
+        for(const key in ids){
+            expect(ids[key]).toBe(1)
+        }
+        expect(Object.keys(ids).length).toBe(blogs.length)
+    })
 })
 
-test('verify existence of unique property id', async () => {
-    const response = await api.get('/api/blogs')
-    const blogs = response.body
-    for(const blog of blogs){
-        expect(blog.id).toBeDefined()
-    }
-    const ids = _.countBy(blogs,blog=>blog.id)
-    for(const key in ids){
-        expect(ids[key]).toBe(1)
-    }
-    expect(Object.keys(ids).length).toBe(blogs.length)
 
+describe('creation of a blog', () => {
+    test('it is possible to add a blog', async ()=> {
+        const newBlog = {
+            title: "New React patterns",
+            author: "Michael Chen",
+            url: "https://reactpatterns.com/",
+            likes: 10
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        const blogsAtTheEnd = await helper.blogsInDb()
+        expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length+1)
+        const savedBlogID = response.body.id
+        const savedBlog = blogsAtTheEnd.find(blog => blog.id.toString() === savedBlogID)
+        expect(savedBlog).toEqual(expect.objectContaining(newBlog))
+    })
 
+    test('default value for likes is 0', async () => {
+        const newBlog = {
+            title: "New React patterns",
+            author: "Michael Chen",
+            url: "https://reactpatterns.com/",
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        const savedBlog = response.body
+        expect(savedBlog.likes).toBe(0)
+    })
+
+    test('not valid blog is not added (url missing)', async () => {
+        const newBlog = {
+            title: "New React patterns",
+            author: "Michael Chen",
+            likes: 12
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
+
+    test('not valid blog is not added (title missing)', async () => {
+        const newBlog = {
+            author: "Michael Chen",
+            url: "https://reactpatterns.com/",
+            likes: 12
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
 })
 
-test('it is possible to add a blog', async ()=> {
-    const newBlog = {
-        title: "New React patterns",
-        author: "Michael Chen",
-        url: "https://reactpatterns.com/",
-        likes: 10
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    const blogsAtTheEnd = await helper.blogsInDb()
-    expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length+1)
-    const savedBlogID = response.body.id
-    const savedBlog = blogsAtTheEnd.find(blog => blog.id.toString() === savedBlogID)
-    expect(savedBlog).toEqual(expect.objectContaining(newBlog))
+describe('deletion of a note', () => {
+    test('succeeds with status code 204', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+        const blogsIds = blogsAtEnd.map(b => b.id)
+        expect(blogsIds).not.toContain(blogToDelete.id)
+    })
 })
 
-test('default value for likes is 0', async () => {
-    const newBlog = {
-        title: "New React patterns",
-        author: "Michael Chen",
-        url: "https://reactpatterns.com/",
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    const savedBlog = response.body
-    expect(savedBlog.likes).toBe(0)
-})
-
-test('not valid blog is not added (url missing)', async () => {
-    const newBlog = {
-        title: "New React patterns",
-        author: "Michael Chen",
-        likes: 12
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
-
-test('not valid blog is not added (title missing)', async () => {
-    const newBlog = {
-        author: "Michael Chen",
-        url: "https://reactpatterns.com/",
-        likes: 12
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
 
 afterAll(async()=>{
     await mongoose.connection.close()
