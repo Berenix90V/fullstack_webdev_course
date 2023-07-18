436c97a625278a3585d4cc23af0cc870fd310f99
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-18 12:08:43 +0300

    implement get all users route

--- notes_app/theory_backend/controllers/users.js
+++ notes_app/theory_backend/controllers/users.js
@@ -4,6 +4,11 @@ import User from '../models/user.js'
 
 const userRouter = express.Router()
 
+userRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
 userRouter.post('/', async (request, response) => {
     const {username, name, password} = request.body
     const saltRounds = 10
