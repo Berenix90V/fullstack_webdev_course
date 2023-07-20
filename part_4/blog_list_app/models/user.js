0d4d6304eae3db0dcf8ad0c777f7b687a5238d4d
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-20 17:02:32 +0300

    exercise 4.15: disabled passwordHash to be shown when get all users

--- part_4/blog_list_app/models/user.js
+++ part_4/blog_list_app/models/user.js
@@ -11,6 +11,7 @@ userSchema.set('toJSON', {
         returnedObject.id = document._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
+        delete returnedObject.passwordHash
     }
 })
 
