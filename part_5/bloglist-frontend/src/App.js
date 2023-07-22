9a664f1440ed1a93b34845436f0d0bcc2df3dea0
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-23 00:45:08 +0300

    exercise 5.1: set frontend form if user not logged in

--- part_5/bloglist-frontend/src/App.js
+++ part_5/bloglist-frontend/src/App.js
@@ -1,24 +1,52 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs( blogs )
+        )
+    }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+    const handleLogin = (event) => {
+        event.preventDefault()
+        console.log('login with ', username, password)
+    }
+
+    if(user){
+        return (
+            <div>
+            <h2>blogs</h2>
+                {blogs.map(blog =>
+                    <Blog key={blog.id} blog={blog} />
+                )}
+            </div>
+        )
+    }
+    else{
+        return (
+            <div>
+                <h2>blogs</h2>
+                    <form onSubmit={handleLogin}>
+                        <div>username <input type="text" value={username} name="Username"
+                                             onChange={({target}) => setUsername(target.value)}
+                        /></div>
+                        <div>password <input type="password" value={password} name="Password"
+                                             onChange={({target}) => setPassword(target.value)}
+                        /></div>
+                        <button type="submit" >Login</button>
+                    </form>
+            </div>
+        )
+    }
+
 }
 
 export default App
