e639d9d9f863b9ef15e35854f34c5d8075d0c919
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-23 19:55:54 +0300

    exercise 5.3: updated useEffect to keep the token

--- part_5/bloglist-frontend/src/components/AddNewBlogForm.js
+++ part_5/bloglist-frontend/src/components/AddNewBlogForm.js
@@ -1,11 +1,14 @@
 const AddNewBlogForm = ({handleAddNewBlog, title, setTitle, author, setAuthor, url, setUrl}) => {
     return(
-        <form onSubmit={handleAddNewBlog}>
-            <p>title <input value={title} onChange={({target}) => setTitle(target.value)}/></p>
-            <p>author <input value={author} onChange={({target}) => setAuthor(target.value)}/></p>
-            <p>url <input value={url} onChange={({target}) => setUrl(target.value)}/></p>
-            <button type="submit">submit</button>
-        </form>
+        <>
+            <h3>Create Blog</h3>
+            <form onSubmit={handleAddNewBlog}>
+                <p>title <input value={title} onChange={({target}) => setTitle(target.value)}/></p>
+                <p>author <input value={author} onChange={({target}) => setAuthor(target.value)}/></p>
+                <p>url <input value={url} onChange={({target}) => setUrl(target.value)}/></p>
+                <button type="submit">create</button>
+            </form>
+        </>
     )
 }
 
