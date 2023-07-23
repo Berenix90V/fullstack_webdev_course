750296ef8dcd07d59d7c3d68d38dea20144dfcf8
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-23 19:09:37 +0300

    exercise 5.3: implemented post method

--- part_5/bloglist-frontend/src/services/blogs.js
+++ part_5/bloglist-frontend/src/services/blogs.js
@@ -1,10 +1,27 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+    token = `Bearer ${token}`
+}
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+const create = (newObject) => {
+    const config ={
+        headers: {Authorization: token}
+    }
+    const request = axios.post(baseUrl, newObject, config)
+    return request.then(response => response.data)
+}
+
+const blogServices = {
+    getAll,
+    create
+}
+export default blogServices
