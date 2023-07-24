35d551794e6b400f5187348fa047f0e12b71e9e2
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-24 22:27:38 +0300

    exercise 5.7: added functionality to show the details

--- part_5/bloglist-frontend/src/components/Blog.js
+++ part_5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({blog}) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import {useState} from "react";
+
+const Blog = ({blog}) => {
+    const [visibleDetails, setVisibleDetails] = useState(false)
+    const hideWhenVisible = {display: visibleDetails? 'none':''}
+    const showWhenVisible = {display: visibleDetails? '':'none'}
+    const toggleVisibility = () => {
+        setVisibleDetails(!visibleDetails)
+    }
+    return(
+        <div>
+            {blog.title} {blog.author}
+            <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+            <button style={showWhenVisible} onClick={toggleVisibility}>hide</button>
+            <div style={showWhenVisible}>
+                <p>url: {blog.url}</p>
+                <p>likes: {blog.likes} <button>like</button> </p>
+                <p>{blog.user.name}</p>
+            </div>
+        </div>
+    )
+}
+
+
+
 
 export default Blog
