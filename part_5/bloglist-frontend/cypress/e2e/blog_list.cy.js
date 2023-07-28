d499321de6f8a418c544b463fa6e6b0c9039e4ab
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-29 01:14:17 +0300

    exercise 5.17: test if form is shown

--- part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
+++ part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Blog list app', function () {
+  beforeEach(function () {
+
+    cy.visit('http://localhost:3000')
+  })
+  it('by default login form is shown', function () {
+    cy.contains('username').get('input').should('exist')
+    cy.contains('password').get('input').should('exist')
+    cy.get('button').should('contain', 'Login')
   })
 })
