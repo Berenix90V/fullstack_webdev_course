812ee3fa6b421c9d94961366a664ef28c43cd470
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-29 01:43:44 +0300

    exercise 5.17: moved urls to config file

--- part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
+++ part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
@@ -1,7 +1,7 @@
 describe('Blog list app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    cy.visit('')
   })
   it('by default login form is shown', function () {
     cy.contains('username').get('input').should('exist')
