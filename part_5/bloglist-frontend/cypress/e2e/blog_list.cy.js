bf09653e63827ad40b1b6649e35f1acc1fcddeda
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-29 01:30:29 +0300

    exercise 5.17: added post request to reset the db in beforeEach

--- part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
+++ part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
@@ -1,6 +1,6 @@
 describe('Blog list app', function () {
   beforeEach(function () {
-
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
   it('by default login form is shown', function () {
