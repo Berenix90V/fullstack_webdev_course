024f76275a64e07a7faf79c4ce265b231073353a
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-28 17:30:32 +0300

    separated common part in beforeEach

--- notes_app/theory_frontend/cypress/e2e/note_app.cy.js
+++ notes_app/theory_frontend/cypress/e2e/note_app.cy.js
@@ -1,11 +1,12 @@
 describe('Note app', function () {
-  it('front page can be opened', function () {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
+  })
+  it('front page can be opened', function () {
     cy.contains('Notes')
     cy.contains('Note app, Department of Science, University of Helsinki 2022')
   })
   it('login form can be opened', function () {
-    cy.visit('http://localhost:3000')
     cy.contains('Log in').click()
   })
 })
