eee775142f5b7fa45b907b6bdae7b2b6648ddde2
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-28 19:30:48 +0300

    using should for style

--- notes_app/theory_frontend/cypress/e2e/note_app.cy.js
+++ notes_app/theory_frontend/cypress/e2e/note_app.cy.js
@@ -26,6 +26,8 @@ describe('Note app', function () {
     cy.get('#login-button').click()
 
     cy.get('.error').should('contain', 'Wrong credentials')
+    cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    cy.get('.error').should('have.css', 'border-style', 'solid')
   })
 
   it('user can login', function () {
