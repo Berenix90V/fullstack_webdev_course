fdc63afd88a9d021df770af206865768b48c5255
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-29 18:37:53 +0300

    exercise 5.20: a user can like a blog

--- part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
+++ part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
@@ -52,7 +52,7 @@ describe('Blog list app', function () {
           })
     })
 
-    it('a new note can be created', function () {
+    it('a new blog can be created', function () {
       cy.contains('create new blog').click()
       cy.get('#title-input').type('Title')
       cy.get('#author-input').type('Author')
@@ -61,7 +61,63 @@ describe('Blog list app', function () {
       cy.contains("A new blog is created", {exact:false}).should('have.css', 'color', 'rgb(0, 128, 0)')
     })
 
+    describe('when more blogs and users exist', function () {
 
+      beforeEach( function () {
+        const user = {
+          username: 'root',
+          name: 'superuser',
+          password: 'password'
+        }
+        cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+        cy.get('#logout-button').click()
+        cy.request('POST', `${Cypress.env('BACKEND')}/login`, {username:'root', password: 'password'})
+            .then(response => {
+              localStorage.setItem('loggedUser', JSON.stringify(response.body))
+              cy.visit('')
+            })
+        const blogCreatedBySuperuser = {
+          title: 'Another Blog',
+          author: 'Oppenheimer',
+          url: 'https://www.blog.com'
+        }
+        cy.request({
+          method: 'POST',
+          url:`${Cypress.env('BACKEND')}/blogs`,
+          body: blogCreatedBySuperuser,
+          headers: {
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem('loggedUser')).token}`
+          }
+        } )
+        cy.get('#logout-button').click()
+        cy.request('POST', `${Cypress.env('BACKEND')}/login`, {username:'mluukkai', password: 'salainen'})
+            .then(response => {
+              localStorage.setItem('loggedUser', JSON.stringify(response.body))
+              cy.visit('')
+            })
+        const blogCreatedByMatti = {
+          title: 'Blog',
+          author: 'Einstein',
+          url: 'https://www.blog.com'
+        }
+        cy.request({
+          method: 'POST',
+          url:`${Cypress.env('BACKEND')}/blogs`,
+          body: blogCreatedByMatti,
+          headers: {
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem('loggedUser')).token}`
+          }
+        } )
+        cy.visit('')
+      })
+
+      it.only('a user can like a blog', function () {
+        cy.get('.blog:first').contains('likes: 0')
+        cy.get('.blog:first').contains('view').click().get('#add-like').click()
+        cy.get('.blog:first').contains('likes: 1')
+      })
+
+    })
 
   })
 })
