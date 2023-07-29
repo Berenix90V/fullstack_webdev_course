dd00d04ec9b38ba4f124ca91ce49b34ffadf1577
Veronica Zanella <zanella.veronica@outlook.it>
2023-07-29 12:56:19 +0300

    exercise 5.17: corrected test to get the single input element in the selected div

--- part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
+++ part_5/bloglist-frontend/cypress/e2e/blog_list.cy.js
@@ -1,11 +1,14 @@
 describe('Blog list app', function () {
+
   beforeEach(function () {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     cy.visit('')
   })
+
   it('by default login form is shown', function () {
-    cy.contains('username').get('input').should('exist')
-    cy.contains('password').get('input').should('exist')
+    cy.contains('username').find('input').should('exist')
+    cy.contains('password').find('input').should('exist')
     cy.get('button').should('contain', 'Login')
   })
+
 })
