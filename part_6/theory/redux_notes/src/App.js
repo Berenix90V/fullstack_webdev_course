6e9193a38a5ba65ed4be73e25bef85b47098ba0d
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-02 00:15:49 +0300

    moved App in a separated file

--- part_6/theory/redux_notes/src/App.js
+++ part_6/theory/redux_notes/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const generateId = () => {
+  Number((Math.random()*1000000).toFixed(0))
+}
+
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
+const App = () => {
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <form onSubmit={addNote}>
+          <input name="note"/>
+          <button type='submit'>add</button>
+        </form>
+        <ul>
+          {store.getState().map(note =>
+              <li
+                  key={note.id}
+                  onClick={() => toggleImportance(note.id)}
+              >
+                {note.content} <strong>{note.important ? 'important' : ''}</strong>
+              </li>
+          )}
+        </ul>
+      </div>
+  )
 }
 
 export default App;
