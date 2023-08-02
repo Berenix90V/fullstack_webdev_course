73aeb691ae103fed242ca895d1dcf0cee53aa410
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-02 14:18:59 +0300

    put list of notes in a separate component

--- part_6/theory/redux_notes/src/App.js
+++ part_6/theory/redux_notes/src/App.js
@@ -1,30 +1,15 @@
 import React from "react";
-import {createNote, toggleImportanceOf} from "./reducers/noteReducer.js";
+import {toggleImportanceOf} from "./reducers/noteReducer.js";
 import {useSelector, useDispatch} from "react-redux";
 import NewNote from "./components/NewNote.js";
+import Notes from "./components/Notes.js";
 
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
       <div>
           <NewNote/>
-        <ul>
-          {notes.map(note =>
-              <li
-                  key={note.id}
-                  onClick={() => toggleImportance(note.id)}
-              >
-                {note.content} <strong>{note.important ? 'important' : ''}</strong>
-              </li>
-          )}
-        </ul>
+          <Notes/>
       </div>
   )
 }
