0642d976c572aac41330ebe8241388b807a0c6a6
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-04 00:24:04 +0300

    added redux toolkit and updated index

--- part_6/theory/redux_notes/src/index.js
+++ part_6/theory/redux_notes/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {combineReducers, createStore} from "redux";
+// import {combineReducers, createStore} from "redux";
+import{configureStore} from '@reduxjs/toolkit'
 import {Provider} from "react-redux";
 import App from './App.js'
 import noteReducer from "./reducers/noteReducer.js";
 import filterReducer from "./reducers/filterReducer.js";
 
-
-const reducer = combineReducers({
-    notes: noteReducer,
-    filter: filterReducer
+const store = configureStore({
+    reducer: {
+        notes: noteReducer,
+        filter: filterReducer
+    }
 })
-const store = createStore(reducer)
+
 console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
