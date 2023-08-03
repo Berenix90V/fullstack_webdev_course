c11d299356d9b58029bf970681cb1a3ff0aeaaa5
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-03 15:28:02 +0300

    implemented visibility functionality

--- part_6/theory/redux_notes/src/components/Notes.js
+++ part_6/theory/redux_notes/src/components/Notes.js
@@ -11,7 +11,18 @@ const Note = ({note, handleClick}) => {
 }
 const Notes = () => {
     const dispatch = useDispatch()
-    const notes = useSelector(state => state.notes)
+    const notes = useSelector(state => {
+        switch(state.filter){
+            case 'ALL':
+                return state.notes
+            case 'IMPORTANT':
+                return state.notes.filter(note=>note.important)
+            case 'NONIMPORTANT':
+                return state.notes.filter(note=>!note.important)
+            default:
+                return state.notes
+        }
+    })
     return(
         <ul>
             {notes.map(note =>
