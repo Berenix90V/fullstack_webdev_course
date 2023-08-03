83cbdf15aacac712db894e873f020998b161601f
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-04 00:43:49 +0300

    refactored reducer with redux toolkit

--- part_6/theory/redux_notes/src/index.js
+++ part_6/theory/redux_notes/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import {combineReducers, createStore} from "redux";
 import{configureStore} from '@reduxjs/toolkit'
 import {Provider} from "react-redux";
 import App from './App.js'
