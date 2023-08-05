adb888d3a8d93780cacf588b494f95334accabbf
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-05 20:03:32 +0300

    exercise 6.14: deleted unused methods

--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,30 +1,19 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const anecdotesAtStart = []
-
-//const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-    return {
-        content: anecdote,
-        votes: 0
-    }
-}
-
 const sortAnecdotes = (anecdotes) => {
     const anecdotesCopy = [...anecdotes]
     anecdotesCopy.sort((a,b) => b.votes-a.votes)
     return anecdotesCopy
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = []
 
 const anecdoteSlice = createSlice({
     name: 'anecdote',
     initialState,
     reducers: {
         createAnecdote(state, action){
-            const anecdotesWithNewAnecdote = state.concat(asObject(action.payload))
+            const anecdotesWithNewAnecdote = state.concat(action.payload)
             return sortAnecdotes(anecdotesWithNewAnecdote)
         },
         incrementVoteOf(state, action){
@@ -33,9 +22,12 @@ const anecdoteSlice = createSlice({
             const changedAnecdote = {...anecdoteToVote, votes: anecdoteToVote.votes+1}
             const updatedAnecdotes = state.map(anecdote => anecdote.id===id? changedAnecdote : anecdote )
             return sortAnecdotes(updatedAnecdotes)
+        },
+        setAnecdotes(state, action) {
+            return action.payload
         }
     }
 })
 
-export const {createAnecdote, incrementVoteOf} = anecdoteSlice.actions
+export const {createAnecdote, incrementVoteOf, setAnecdotes} = anecdoteSlice.actions
 export default anecdoteSlice.reducer
