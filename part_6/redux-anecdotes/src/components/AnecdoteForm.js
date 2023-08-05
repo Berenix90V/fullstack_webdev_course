9516b562c9fd14f81462c8bb35d0a52ac2bcac75
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-05 23:32:26 +0300

    exercise 6.14: added axios method in handling anecdote addition

--- part_6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part_6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,19 +1,21 @@
-import {createAnecdote} from "../reducers/anecdoteReducer";
+import {addAnecdote} from "../reducers/anecdoteReducer";
 import {useDispatch} from "react-redux";
 import {setNotification, unsetNotification} from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
-    const addAnecdote = (event) => {
+    const handleAddAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(addAnecdote(newAnecdote))
         dispatch(setNotification('Added new anecdote'))
         setTimeout(()=>dispatch(unsetNotification()), 5000)
     }
     return (
-        <form onSubmit={addAnecdote}>
+        <form onSubmit={handleAddAnecdote}>
             <div><input name="anecdote"/></div>
             <button type="submit">create</button>
         </form>
