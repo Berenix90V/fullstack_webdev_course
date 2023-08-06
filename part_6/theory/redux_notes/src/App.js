598b3fef654f5eb4ebc673a464c0d49b16e99a03
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-06 23:28:14 +0300

    logic related to how initialize the notes separated from the react component App

--- part_6/theory/redux_notes/src/App.js
+++ part_6/theory/redux_notes/src/App.js
@@ -3,14 +3,12 @@ import NewNote from "./components/NewNote.js";
 import Notes from "./components/Notes.js";
 import VisibilityFilter from "./components/VisibilityFilter.js";
 import {useDispatch} from "react-redux";
-import noteServices from "./services/notes.js";
-import {setNotes} from "./reducers/noteReducer.js";
+import {initializeNotes, setNotes} from "./reducers/noteReducer.js";
 
 const App = () => {
     const dispatch = useDispatch()
     useEffect(() => {
-        noteServices
-            .getAll().then(notes => dispatch(setNotes(notes)))
+        dispatch(initializeNotes())
     }, [dispatch])
     return (
         <div>
