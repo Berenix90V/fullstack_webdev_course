e1cfedfc9b8494dcb1f475bd682df9ec058acf3d
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-06 23:44:31 +0300

    put store initialization in a separated file

--- part_6/theory/redux_notes/src/index.js
+++ part_6/theory/redux_notes/src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import{configureStore} from '@reduxjs/toolkit'
 import {Provider} from "react-redux";
 import App from './App.js'
-import noteReducer from "./reducers/noteReducer.js";
-import filterReducer from "./reducers/filterReducer.js";
+import store from "./config/store.js";
 
-const store = configureStore({
-    reducer: {
-        notes: noteReducer,
-        filter: filterReducer
-    }
-})
-
-console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
