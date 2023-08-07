10dadde173d63494e8f32df49eb6a9cc699fc790
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-08 01:18:42 +0300

    exercise 6.23: set up notification for adding an anecdote

--- part_6/query-anecdotes/src/components/AnecdoteForm.js
+++ part_6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,8 +1,10 @@
 import {useMutation, useQueryClient} from "react-query";
 import anecdoteServices from "../requests";
+import {useNotificationDispatch} from "../notificationContext";
 
 const AnecdoteForm = () => {
     const queryClient = useQueryClient()
+    const dispatch = useNotificationDispatch()
     const newNoteMutation = useMutation(anecdoteServices.createNew, {
         onSuccess: (newAnecdote) => {
             const anecdotes = queryClient.getQueryData('anecdotes')
@@ -14,6 +16,12 @@ const AnecdoteForm = () => {
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
         newNoteMutation.mutate({content, votes:0})
+
+        dispatch({
+            type: 'SET_NOTIFICATION',
+            payload: `Added new anecdote ${content}`
+        })
+        setTimeout(()=>dispatch({type: 'UNSET_NOTIFICATION', payload: ''}), 5000)
     }
 
     return (
