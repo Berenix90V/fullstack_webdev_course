f11e5cecb5d7de06aef3fd2827c8651af0ddbd16
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-11 20:21:16 +0300

    set up page components

--- part_7/theory/notes/src/index.js
+++ part_7/theory/notes/src/index.js
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
-import {QueryClient, QueryClientProvider} from "react-query";
-
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
+const Home = () => (
+    <div>
+        <h2>TKTL notes app</h2>
+        <div>
+            Lorem ipsum
+        </div>
+    </div>
 )
+
+const Notes = () => (
+    <div>
+        <h2>Notes</h2>
+        <div>
+            <ul>
+                <li>HTML is easy</li>
+                <li>Browser can execute javascript</li>
+                <li>Browser talks to server with HTML</li>
+            </ul>
+        </div>
+    </div>
+)
+
+const Users = () => (
+    <div><h2>Users</h2></div>
+)
+
+const App = () => {
+    const [page, setPage] = useState('home')
+    const toPage = (page) => (event) => {
+        event.preventDefault()
+        setPage(page)
+    }
+    const content = () => {
+        if(page==='home'){
+            return <Home/>
+        } else if (page === 'notes') {
+            return <Notes />
+        } else if (page === 'users') {
+            return <Users />
+        }
+    }
+    const padding = {
+        padding: 5
+    }
+
+    return (
+        <div>
+            <div>
+                <a href="" onClick={toPage('home')} style={padding}>home</a>
+                <a href="" onClick={toPage('notes')} style={padding}>notes</a>
+                <a href="" onClick={toPage('users')} style={padding}>users</a>
+            </div>
+            {content()}
+        </div>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
