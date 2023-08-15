9550e96f1bc732326ca704794a224975d2bbe13b
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-15 23:36:24 +0300

    implemented changing anecdote

--- part_7/theory/class-components/src/App.js
+++ part_7/theory/class-components/src/App.js
@@ -19,6 +19,13 @@ class App extends React.Component {
       })
   }
 
+  handleClick = () => {
+    const current = Math.floor(
+      Math.random()*this.state.anecdotes.length
+    )
+    this.setState({current})
+  }
+
   render(){
     if(this.state.anecdotes.length ===0) {
       return <div>no anecdotes ...</div>
@@ -29,7 +36,7 @@ class App extends React.Component {
         <div>
           {this.state.anecdotes[this.state.current].content}
         </div>
-        <button>next</button>
+        <button onClick={this.handleClick}>next</button>
       </div>
     )
   }
