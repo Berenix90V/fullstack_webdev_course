a97da4efd9493906f53fe3a7ef8895d5b3ee132d
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-15 23:16:38 +0300

    set up basic React app with old class syntax

---
+++ part_7/theory/class-components/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return (
+        <div>
+          <h1>anecdote of the day</h1>
+        </div>
+    )
+  }
+}
+
+export default App;
