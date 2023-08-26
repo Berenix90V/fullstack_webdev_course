16c6547eacc0900eb5116511fb565ac3c6347822
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-26 18:32:24 +0300

    exercise 7.14: implemented view to  visualize users and blogs created by them

--- part_7/blog-list/blog-list-frontend-redux/src/components/Users.js
+++ part_7/blog-list/blog-list-frontend-redux/src/components/Users.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react'
+import userService from '../services/users'
+
+const User = ({ user }) => {
+    const blogs = user.blogs.length
+    return (
+        <tr>
+            <td>{user.name}</td>
+            <td>{blogs}</td>
+        </tr>
+    )
+}
+
 const Users = () => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        const initializeUsers = async () => {
+            const allUsers =  await userService.getAll()
+            setUsers(allUsers)
+        }
+        initializeUsers()
+    },[])
+
     return(
         <>
-            users
+            <h2>Users</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>blogs created</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => <User key={user.id} user={user} />)}
+                </tbody>
+
+            </table>
+
         </>
     )
 }
