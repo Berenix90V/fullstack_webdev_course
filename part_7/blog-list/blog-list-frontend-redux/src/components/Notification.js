d42d2940be745afef19f68614b0339728c8feafd
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-28 22:46:37 +0300

    exercise 7.20: styled Notifications

--- part_7/blog-list/blog-list-frontend-redux/src/components/Notification.js
+++ part_7/blog-list/blog-list-frontend-redux/src/components/Notification.js
@@ -1,9 +1,14 @@
 import { useSelector } from 'react-redux'
+import { Alert } from 'react-bootstrap'
 
 const Notification = () => {
     const { message, className } = useSelector((state) => state.notifications)
     if (message !== '') {
-        return <div className={ className }> { message } </div>
+        return (
+            <Alert variant={className}>
+                {message}
+            </Alert>
+        )
     }
 }
 
