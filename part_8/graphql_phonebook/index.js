f25a8b3ad8f3ecb6258b5cc2ed2ab847e7990404
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-30 18:10:05 +0300

    enum

--- part_8/graphql_phonebook/index.js
+++ part_8/graphql_phonebook/index.js
@@ -38,9 +38,14 @@ const typeDefs = `
         id: ID!
     }
     
+    enum YesNo {
+        YES
+        NO
+    }
+    
     type Query {
         personCount: Int!
-        allPersons: [Person!]!
+        allPersons(phone:YesNo): [Person!]!
         findPerson(name: String!): Person
     }
     
@@ -51,7 +56,11 @@ const typeDefs = `
             street: String!
             city: String!
         ): Person
-}
+        editNumber(
+            name: String!
+            phone: String!
+        )
+    }
 `
 
 const resolvers = {
@@ -82,6 +91,15 @@ const resolvers = {
             const person = {...args, id: uuid()}
             persons = persons.concat(person)
             return person
+        },
+        editNumber: (root, args) => {
+            const person = persons.find(p => p.name === args.name)
+            if(!person){
+                return null
+            }
+            const updatedPerson = {...person, phone: args.phone}
+            persons = persons.map(p => p.name === args.name? updatedPerson : p)
+            return updatedPerson
         }
     }
 }
