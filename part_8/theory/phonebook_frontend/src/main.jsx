c56d05e9df1dd0aaab2eea9437801a5303d29c86
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-31 09:43:02 +0300

    rendered client accessible with apollo provider

--- part_8/theory/phonebook_frontend/src/main.jsx
+++ part_8/theory/phonebook_frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
+import {ApolloClient, gql, InMemoryCache, ApolloProvider} from "@apollo/client";
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000',
@@ -27,7 +27,7 @@ client.query({ query })
     })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
