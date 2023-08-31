82dce8c8264cfcfbbf0ebea2ec949d1814774586
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-31 12:36:29 +0300

    set up notification on wrong creation of a person

--- part_8/theory/phonebook_frontend/src/App.jsx
+++ part_8/theory/phonebook_frontend/src/App.jsx
@@ -2,18 +2,39 @@ import {useQuery} from "@apollo/client";
 import Persons from "./components/Persons.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import {ALL_PERSONS} from "./queries.js";
+import {useState} from "react";
+
+const Notify = ({errorMessage}) => {
+    if(!errorMessage){
+        return null
+    }
+    return (
+        <div style={{color: 'red'}}>
+            {errorMessage}
+        </div>
+    )
+}
 
 function App() {
-  const result = useQuery(ALL_PERSONS)
-  if(result.loading){
-    return <div>loading...</div>
-  }
-  return (
-    <div>
-      <Persons persons={result.data.allPersons}></Persons>
-      <PersonForm/>
-    </div>
-  )
+    const [errorMessage, setErrorMessage] = useState(null)
+    const result = useQuery(ALL_PERSONS)
+
+    if(result.loading){
+        return <div>loading...</div>
+    }
+    const notify = (message) => {
+        setErrorMessage(message)
+        setTimeout(() => {
+            setErrorMessage(null)
+        }, 10000)
+    }
+    return (
+        <div>
+            <Notify errorMessage={errorMessage}/>
+            <Persons persons={result.data.allPersons}></Persons>
+            <PersonForm setError={notify} />
+        </div>
+    )
 }
 
 export default App
