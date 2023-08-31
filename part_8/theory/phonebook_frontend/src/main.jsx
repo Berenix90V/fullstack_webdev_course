0db39a6a2f8ace9305557761f9d51c858c47c04b
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-31 09:42:24 +0300

    basic app with query to apollo client

--- part_8/theory/phonebook_frontend/src/main.jsx
+++ part_8/theory/phonebook_frontend/src/main.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache:new InMemoryCache()
+})
+
+const query = gql`
+    query {
+        allPersons {
+            name,
+            phone,
+            address {
+                street,
+                city
+            }
+            id
+        }
+    }
+`
+client.query({ query })
+    .then((response) => {
+        console.log(response.data)
+    })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
