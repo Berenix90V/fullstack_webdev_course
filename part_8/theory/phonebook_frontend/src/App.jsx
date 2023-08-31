ff177f2f95f306be189eb36f9a71a463180b240d
Veronica Zanella <zanella.veronica@outlook.it>
2023-08-31 10:51:03 +0300

    making queries

--- part_8/theory/phonebook_frontend/src/App.jsx
+++ part_8/theory/phonebook_frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import {gql, useQuery} from "@apollo/client";
 
+const ALL_PERSONS = gql`
+query {
+  allPersons {
+    name
+    phone
+    id
+  }
+}
+`
 function App() {
+  const result = useQuery(ALL_PERSONS)
+  if(result.loading){
+    return <div>loading...</div>
+  }
   return (
-    <>
-    </>
+    <div>
+      {result.data.allPersons.map(p=>p.name).join(', ')}
+    </div>
   )
 }
 
