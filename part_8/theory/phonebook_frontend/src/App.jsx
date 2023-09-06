4e46e4883831fbec84b980b356766d2fddf33ab6
Veronica Zanella <zanella.veronica@outlook.it>
2023-09-06 12:31:36 +0300

    implemented login form component

--- part_8/theory/phonebook_frontend/src/App.jsx
+++ part_8/theory/phonebook_frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import {useQuery} from "@apollo/client";
+import {useApolloClient, useQuery} from "@apollo/client";
 import Persons from "./components/Persons.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import {ALL_PERSONS} from "./queries.js";
 import {useState} from "react";
 import PhoneForm from "./components/PhoneForm.jsx";
+import LoginForm from "./components/LoginForm.jsx";
 
 const Notify = ({errorMessage}) => {
     if(!errorMessage){
@@ -20,6 +21,7 @@ function App() {
     const [errorMessage, setErrorMessage] = useState(null)
     const result = useQuery(ALL_PERSONS)
     const [token, setToken] = useState(null)
+    const client = useApolloClient()
 
     if(result.loading){
         return <div>loading...</div>
@@ -29,6 +31,11 @@ function App() {
         setTimeout(() => {
             setErrorMessage(null)
         }, 10000)
+    }
+    const logout = () => {
+        setToken(null)
+        localStorage.clear()
+        client.resetStore()
     }
 
     if(!token){
@@ -47,6 +54,7 @@ function App() {
     return (
         <div>
             <Notify errorMessage={errorMessage}/>
+            <button onClick={logout}>logout</button>
             <Persons persons={result.data.allPersons}></Persons>
             <PersonForm setError={notify} />
             <PhoneForm setError={notify} />
