dd3cfc0ff4f38dcbe76ee7e0c1649a28b55c7975
Veronica Zanella <zanella.veronica@outlook.it>
2023-09-06 17:29:45 +0300

    exercise 8.18: implemented login and logout logic

--- part_8/library/library-frontend/src/App.js
+++ part_8/library/library-frontend/src/App.js
@@ -2,25 +2,53 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from "./components/LoginForm";
+import {useApolloClient} from "@apollo/client";
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+    const [page, setPage] = useState('authors')
+    const [token, setToken] = useState(null)
+    const client = useApolloClient()
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-      </div>
+    const logout = () => {
+        setToken(null)
+        localStorage.clear()
+        client.resetStore()
+    }
 
-      <Authors show={page === 'authors'} />
+    if(!token){
+        return(
+            <>
+                <div>
+                    <button onClick={() => setPage('authors')}>authors</button>
+                    <button onClick={() => setPage('books')}>books</button>
+                    <button onClick={() => setPage('login')}>login</button>
+                </div>
+                <Authors show={page === 'authors'} />
 
-      <Books show={page === 'books'} />
+                <Books show={page === 'books'} />
 
-      <NewBook show={page === 'add'} />
-    </div>
-  )
+                <LoginForm  show={page === 'login'} setToken={setToken}/>
+            </>
+        )
+    }
+
+    return (
+        <div>
+            <button onClick={logout}>logout</button>
+            <div>
+                <button onClick={() => setPage('authors')}>authors</button>
+                <button onClick={() => setPage('books')}>books</button>
+                <button onClick={() => setPage('add')}>add book</button>
+            </div>
+
+            <Authors show={page === 'authors'} />
+
+            <Books show={page === 'books'} />
+
+            <NewBook show={page === 'add'} />
+        </div>
+    )
 }
 
 export default App
