8ef4d20ce607c5c21a26e25341529961fc0b2c83
Veronica Zanella <zanella.veronica@outlook.it>
2023-09-08 19:30:22 +0300

    updated starting server

--- part_8/theory/graphql_phonebook/index.js
+++ part_8/theory/graphql_phonebook/index.js
@@ -1,13 +1,16 @@
 import {ApolloServer} from "@apollo/server";
-import {startStandaloneServer} from "@apollo/server/standalone";
-import {GraphQLError} from 'graphql'
 import mongoose from "mongoose";
 import config from "./utils/config.js";
-import Person from "./models/person.js"
+import express from 'express'
 import User from "./models/user.js";
-import jsonwebtoken from 'jsonwebtoken'
 import typeDefs from "./schema.js";
 import resolvers from "./resolvers.js";
+import * as http from "http";
+import {ApolloServerPluginDrainHttpServer} from "@apollo/server/src/plugin/drainHttpServer/index.js";
+import {makeExecutableSchema} from "@graphql-tools/schema";
+import {expressMiddleware} from "@apollo/server/express4";
+import jwt from "jsonwebtoken";
+import cors from 'cors'
 
 mongoose.set('strictQuery', false)
 
@@ -20,22 +23,37 @@ mongoose.connect(config.mongodb_url)
     })
 
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-})
+const start = async () => {
+    const app = express()
+    const httpServer = http.createServer(app)
+    const server = new ApolloServer({
+        schema: makeExecutableSchema({typeDefs, resolvers}),
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    })
+    await server.start()
 
+    app.use(
+        '/',
+        cors(),
+        express.json(),
+        expressMiddleware(server, {
+            context: async({req})=> {
+                const auth = req? req.headers.authorization : null
+                if(auth && auth.startsWith('Bearer ')){
+                    const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+                    const currentUser = await User.findById(decodedToken.id).populate(
+                        'friends'
+                    )
+                    return { currentUser }
+                }
+            }
+        })
+    )
+    const PORT = 4000
 
-startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({req,res}) => {
-        const auth = req ? req.headers.authorization : null
-        if(auth && auth.startsWith('Bearer ')){
-            const decodedToken = jsonwebtoken.verify(auth.substring(7), process.env.JWT_SECRET)
-            const currentUser = await User.findById(decodedToken.id).populate('friends')
-            return {currentUser}
-        }
-    }
-}).then(({ url }) => {
-    console.log(`Server ready at ${url}`)
-})
+    httpServer.listen(PORT, () =>
+        console.log(`Server is now running on http://localhost:${PORT}`)
+    )
+}
+
+start()
